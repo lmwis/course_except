2d0359f4005ce7b7eddfaf3a73fa142b61b08564
lmwis <detectiveygn@163.com>
2020-12-20 17:58:34 +0800

    feat<api call>:update api counter version

--- src/main/java/com/fehead/course/authentication/FeheadAuthenticationSuccessHandler.java
+++ src/main/java/com/fehead/course/authentication/FeheadAuthenticationSuccessHandler.java
@@ -8,7 +8,6 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
